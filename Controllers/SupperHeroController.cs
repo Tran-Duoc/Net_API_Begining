bfa86e8bf6e9c55d1f816306a000f17783e520ed
Tran Duoc <duoctran2527@gmail.com>
2023-08-29 17:45:32 +0700

    begining .net api

---
+++ Controllers/SupperHeroController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupperHeroAPI.Models;
+
+namespace SupperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupperHeroController : ControllerBase
+    {
+
+         public static List<SupperHero> supperheros = new List<SupperHero> {
+                new SupperHero {
+                    Id = 1,
+                    FirstName = "Tran",
+                    LastName = "Duoc",
+                    Name = "Tran Minh Duoc",
+                    Place = "Viet Nam"
+                },
+                new SupperHero {
+                    Id = 2,
+                    FirstName = "Minh",
+                    LastName = "Thu",
+                    Name = "Minh Thu",
+                    Place = "Viet Nam"
+                }
+            };
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<SupperHero>>> GetAllHero()
+        {
+            return Ok(supperheros);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SupperHero>>> GetSignleHero (int id)
+        {
+            var hero = supperheros.Find(x => x.Id == id);
+            if(hero == null)
+            {
+                return BadRequest("Item not found");
+
+            }
+            return Ok(hero);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<SupperHero>>> AddHero (SupperHero hero)
+        {
+            supperheros.Add(hero);
+            return Ok(supperheros); 
+        }
+
+    }
+}
